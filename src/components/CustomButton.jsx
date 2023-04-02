78296453cfe95cf914b51f62f764be39047a9cc4
Julio Perez <julioperez.dev@gmail.com>
2023-04-02 20:48:20 -0300

    setup project

---
+++ src/components/CustomButton.jsx
@@ -0,0 +1,27 @@
+import { useSnapshot } from "valtio"
+import state from "../store"
+
+
+export default function CustomButton({type, title, customStyles, handleClick}){
+    
+    const snap = useSnapshot(state);
+
+    const generateStyle = () =>{
+        if(type === 'filled'){
+            return {
+                background: snap.color,
+                color: '#fff'
+            }
+        }
+    }
+    
+    return(
+        <button
+        className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`}
+        style={generateStyle(type)}
+        onClick={handleClick}
+        >
+            {title}
+        </button>
+    )
+}
